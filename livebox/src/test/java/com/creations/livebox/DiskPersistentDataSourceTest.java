88bb5fa3bc06cf5354b57b1be46cbd02d165ebf0
Sérgio Serra <sergio.serra@olx.com>
2018-08-30 17:30:49 +0100

    Add unit tests for DiskPermanentDataSource.

--- livebox/src/test/java/com/creations/livebox/DiskPersistentDataSourceTest.java
+++ livebox/src/test/java/com/creations/livebox/DiskPersistentDataSourceTest.java
@@ -1,6 +1,8 @@
 package com.creations.livebox;
 
 import com.creations.livebox.datasources.disk.DiskPersistentDataSource;
+import com.creations.livebox.serializers.LiveboxGsonSerializer;
+import com.creations.livebox.serializers.Serializer;
 import com.creations.livebox.util.Logger;
 import com.creations.livebox.util.Optional;
 import com.google.gson.reflect.TypeToken;
@@ -11,8 +13,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import okio.BufferedSource;
+import okio.Okio;
 
 /**
  * Unit tests for {@link DiskPersistentDataSourceTest}
@@ -24,6 +30,9 @@ public class DiskPersistentDataSourceTest {
     @Before
     public void setup() {
         Logger.disable();
+        LiveboxBuilder.persistentCacheConfig(new DiskPersistentDataSource.Config(
+                new File("src/test/resources")
+        ));
     }
 
     @Test
@@ -31,9 +40,6 @@ public class DiskPersistentDataSourceTest {
 
         // Setup
         final String key = "1000";
-        LiveboxBuilder.persistentCacheConfig(new DiskPersistentDataSource.Config(
-                new File("src/test/resources")
-        ));
 
         final List<String> values = new ArrayList<>();
         values.add("one");
@@ -53,11 +59,30 @@ public class DiskPersistentDataSourceTest {
         // Verify
         Assert.assertTrue(newBagOpt.isPresent());
         Assert.assertEquals(bag, newBagOpt.get());
-
     }
 
     @Test
     public void saveAndReadBigJsonToDisk() {
+
+        // Setup
+        InputStream is = getClass().getClassLoader().getResourceAsStream("bigJson.json");
+        BufferedSource source = Okio.buffer(Okio.source(is));
+
+        TypeToken<List<Bag<String>>> bagType = new TypeToken<List<Bag<String>>>() {
+        };
+        final Serializer<List<Bag<String>>> serializer = LiveboxGsonSerializer.create(bagType);
+        final List<Bag<String>> bags = serializer.deserialize(source);
+
+        // Exercise
+        final String key = "2000";
+        final DiskPersistentDataSource<List<Bag<String>>, List<Bag<String>>> dataSource
+                = DiskPersistentDataSource.create(bagType.getType());
+        dataSource.save(key, bags);
+        final Optional<List<Bag<String>>> newBagOpt = dataSource.read(key);
+
+        // Verify
+        Assert.assertTrue(newBagOpt.isPresent());
+        Assert.assertEquals(bags, newBagOpt.get());
 
     }
 
